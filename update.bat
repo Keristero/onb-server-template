ec01286a4d42b8ff0fd32e2c70f31e04d930e80e
Keristero <10967222+Keristero@users.noreply.github.com>
2023-09-19 16:36:15 +1200

    Update update.bat

--- update.bat
+++ update.bat
@@ -3,23 +3,23 @@
 where rustc >nul 2>&1
 if %errorlevel% == 0 (
     echo Rust is installed
-    set /p installrust="Do you want to try update it anyway? (y/n)? "
+    set /p installrust="Do you want to update it anyway? (y/n): "
 ) else (
-    set /p installrust="y"
-    echo Rust is not installed, press any key to download and install
-    PAUSE
+    set /p installrust="Rust is not installed. Do you want to install it? (y/n): "
 )
 
 if /I "%installrust%"=="y" (
     curl https://win.rustup.rs -sSf -o sources/rustup-init.exe
     call "sources/rustup-init.exe"
-    refreshenv
+    echo Rust has been installed or updated. Please close this window and try running the script again
+    pause
+    exit
 )
 
-echo Downloading latest server source and ezlibs
+echo Downloading the latest server source and ezlibs
 git submodule update --recursive --remote
 
-echo Compiling latest server
+echo Compiling the latest server
 cd .\sources\Scriptable-OpenNetBattle-Server
 
 cargo build --release
@@ -27,4 +27,6 @@ cd ..\..
 copy .\sources\Scriptable-OpenNetBattle-Server\target\release\net_battle_server.exe .\server\net_battle_server.exe
 if %errorlevel% == 0 (
     echo net_battle_server.exe is up to date.
-)
+) 
+
+PAUSE
