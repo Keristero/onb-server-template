da062c28c7545bf397fe880d0b43c4cde6c4ffd8
Keristero <keristero@gmail.com>
2024-02-18 21:40:26 +1300

    update docs

--- server/scripts/netflowsbeta/nodes/misc/unpack.lua
+++ server/scripts/netflowsbeta/nodes/misc/unpack.lua
@@ -10,7 +10,7 @@ local colors = {
 
 return {
     global_object = 'table',
-    description = 'specify an object type parameter, its values will be added to the context, you can also specify new name mappings for each unpacked value. the original table will be removed from the context.',
+    description = 'specify an table type value from the context, its values will be added to the context, you can also specify new name mappings for each unpacked value. the original table will be removed from the context.',
     override_func = function (node,context)
         if context.parameter_name and context[context.parameter_name] then
             for key, value in pairs(context[context.parameter_name]) do
