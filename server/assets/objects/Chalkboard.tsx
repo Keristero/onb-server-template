c790e11119ffe0451cf81e2dae9373baa8e343cc
Keristero <keristero@gmail.com>
2024-03-14 17:45:05 +1300

    add flavourtext example

---
+++ server/assets/objects/Chalkboard.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.10.2" name="Chalkboard" tilewidth="110" tileheight="110" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="0" y="58"/>
+ <image source="Chalkboard.png" width="110" height="110"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="110" y="110">
+    <polygon points="0,0 -27.5,0 -109.529,-41.5148 -110,-55 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
